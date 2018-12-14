01177ad708fd8cff7ee9d6a850b3cbd58d2b958f
Cobus Smit <cobus.smit@chillisoft.co.za>
2018-12-14 14:48:23 +0200

    feat: added reverse calculation 'ToInteger'

--- source/roman.numerals.tests/RomanTests.cs
+++ source/roman.numerals.tests/RomanTests.cs
@@ -1,132 +1,252 @@
 using NUnit.Framework;
-using roman.numerals;
 using System;
 
-namespace Tests
+namespace roman.numerals.tests
 {
+    [TestFixture]
     public class RomanTests
     {
-        [Test]
-        public void Given0_ReturnsEmptyString()
-        {
-            //arrange
-            var roman = new Roman();
-            //act
-            var actual = roman.ToRomanNumerals(0);
-            //assert
-            Assert.That(actual, Is.Empty);
-        }
-
-        [Test]
-        public void GivenNegative_ShouldThrow()
-        {
-            //arrange
-            var roman = new Roman();
-            //act
-            //assert
-            Assert.That(()=> roman.ToRomanNumerals(-1), Throws.TypeOf<ArgumentOutOfRangeException>()
-                .With.Property(nameof(ArgumentOutOfRangeException.ParamName))
-                .EqualTo("decimalValue"));
-        }
-
         [TestFixture]
-        public class SingleLetters
-        {
-            [TestCase(1, "I")]
-            [TestCase(5, "V")]
-            [TestCase(10, "X")]
-            [TestCase(50, "L")]
-            [TestCase(100, "C")]
-            [TestCase(500, "D")]
-            [TestCase(1000, "M")]
-            public void GivenX_ReturnsY(int number, string expected)
-            {
-                //arrange
-                var roman = new Roman();
-                //act
-                var actual = roman.ToRomanNumerals(number);
-                //assert
-                Assert.That(actual, Is.EqualTo(expected));
-            }
-        }
-
-        [TestFixture]
-        public class From1To5
+        public class ToRomanNumerals
         {
             [Test]
-            public void Given2_ReturnsII()
+            public void Given0_ReturnsEmptyString()
             {
                 //arrange
                 var roman = new Roman();
                 //act
-                var actual = roman.ToRomanNumerals(2);
+                var actual = roman.ToRomanNumerals(0);
                 //assert
-                Assert.That(actual, Is.EqualTo("II"));
+                Assert.That(actual, Is.Empty);
             }
 
             [Test]
-            public void Given3_ReturnsIII()
+            public void GivenNegative_ShouldThrow()
             {
                 //arrange
                 var roman = new Roman();
                 //act
-                var actual = roman.ToRomanNumerals(3);
                 //assert
-                Assert.That(actual, Is.EqualTo("III"));
+                Assert.That(() => roman.ToRomanNumerals(-1), Throws.TypeOf<ArgumentOutOfRangeException>()
+                    .With.Property(nameof(ArgumentOutOfRangeException.ParamName))
+                    .EqualTo("decimalValue"));
+            }
+
+            [TestFixture]
+            public class SingleLetters
+            {
+                [TestCase(1, "I")]
+                [TestCase(5, "V")]
+                [TestCase(10, "X")]
+                [TestCase(50, "L")]
+                [TestCase(100, "C")]
+                [TestCase(500, "D")]
+                [TestCase(1000, "M")]
+                public void GivenX_ReturnsY(int number, string expected)
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToRomanNumerals(number);
+                    //assert
+                    Assert.That(actual, Is.EqualTo(expected));
+                }
+            }
+
+            [TestFixture]
+            public class From1To5
+            {
+                [Test]
+                public void Given2_ReturnsII()
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToRomanNumerals(2);
+                    //assert
+                    Assert.That(actual, Is.EqualTo("II"));
+                }
+
+                [Test]
+                public void Given3_ReturnsIII()
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToRomanNumerals(3);
+                    //assert
+                    Assert.That(actual, Is.EqualTo("III"));
+                }
+
+                [Test]
+                public void Given4_ReturnsIV()
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToRomanNumerals(4);
+                    //assert
+                    Assert.That(actual, Is.EqualTo("IV"));
+                }
+            }
+
+            [TestFixture]
+            public class CherryPickedValues
+            {
+                [TestCase(6, "VI")]
+                [TestCase(7, "VII")]
+                [TestCase(8, "VIII")]
+                [TestCase(9, "IX")]
+
+                [TestCase(18, "XVIII")]
+                [TestCase(40, "XL")]
+                [TestCase(41, "XLI")]
+                [TestCase(44, "XLIV")]
+                [TestCase(49, "XLIX")]
+
+                [TestCase(99, "XCIX")]
+
+                [TestCase(1994, "MCMXCIV")]
+
+                [TestCase(3999, "MMMCMXCIX")]
+                public void GivenX_ReturnsY(int number, string expected)
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToRomanNumerals(number);
+                    //assert
+                    Assert.That(actual, Is.EqualTo(expected));
+                }
             }
 
             [Test]
-            public void Given4_ReturnsIV()
+            public void Given4000_ShouldThrow()
             {
                 //arrange
                 var roman = new Roman();
                 //act
-                var actual = roman.ToRomanNumerals(4);
                 //assert
-                Assert.That(actual, Is.EqualTo("IV"));
+                Assert.That(() => roman.ToRomanNumerals(4000), Throws.TypeOf<ArgumentOutOfRangeException>()
+                    .With.Property(nameof(ArgumentOutOfRangeException.ParamName))
+                    .EqualTo("decimalValue"));
             }
         }
 
         [TestFixture]
-        public class CherryPickedValues
+        public class ToInteger
         {
-            [TestCase(6, "VI")]
-            [TestCase(7, "VII")]
-            [TestCase(8, "VIII")]
-            [TestCase(9, "IX")]
-
-            [TestCase(18, "XVIII")]
-            [TestCase(40, "XL")]
-            [TestCase(41, "XLI")]
-            [TestCase(44, "XLIV")]
-            [TestCase(49, "XLIX")]
-
-            [TestCase(99, "XCIX")]
-
-            [TestCase(1994, "MCMXCIV")]
-
-            [TestCase(3999, "MMMCMXCIX")]
-            public void GivenX_ReturnsY(int number, string expected)
+            [Test]
+            public void GivenEmptyString_Returns0()
             {
                 //arrange
                 var roman = new Roman();
                 //act
-                var actual = roman.ToRomanNumerals(number);
+                var actual = roman.ToInteger("");
                 //assert
-                Assert.That(actual, Is.EqualTo(expected));
+                Assert.That(actual, Is.EqualTo(0));
             }
-        }
 
-        [Test]
-        public void Given4000_ShouldThrow()
-        {
-            //arrange
-            var roman = new Roman();
-            //act
-            //assert
-            Assert.That(() => roman.ToRomanNumerals(4000), Throws.TypeOf<ArgumentOutOfRangeException>()
-                .With.Property(nameof(ArgumentOutOfRangeException.ParamName))
-                .EqualTo("decimalValue"));
+
+            [Test]
+            public void GivenInvalidString_Throws()
+            {
+                //arrange
+                var roman = new Roman();
+                //act
+                //assert
+                Assert.That(() => roman.ToInteger("FOOBAR"), Throws.TypeOf<ArgumentOutOfRangeException>()
+                    .With.Property(nameof(ArgumentOutOfRangeException.ParamName))
+                    .EqualTo("romanNumerals"));
+            }
+
+            [TestFixture]
+            public class SingleLetters
+            {
+                [TestCase("I", 1)]
+                [TestCase("V", 5)]
+                [TestCase("X", 10)]
+                [TestCase("L", 50)]
+                [TestCase("C", 100)]
+                [TestCase("D", 500)]
+                [TestCase("M", 1000)]
+                public void GivenX_ReturnsY(string numerals, int expected)
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToInteger(numerals);
+                    //assert
+                    Assert.That(actual, Is.EqualTo(expected));
+                }
+            }
+
+            [TestFixture]
+            public class From1To5
+            {
+                [Test]
+                public void GivenII_Returns2()
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToInteger("II");
+                    //assert
+                    Assert.That(actual, Is.EqualTo(2));
+                }
+
+                [Test]
+                public void GivenIII_Returns3()
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToInteger("III");
+                    //assert
+                    Assert.That(actual, Is.EqualTo(3));
+                }
+
+                [Test]
+                public void GivenIV_Returns4()
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToInteger("IV");
+                    //assert
+                    Assert.That(actual, Is.EqualTo(4));
+                }
+            }
+
+            [TestFixture]
+            public class CherryPickedValues
+            {
+                [TestCase("VI", 6)]
+                [TestCase("VII", 7)]
+                [TestCase("VIII", 8)]
+                [TestCase("IX", 9)]
+
+                [TestCase("XVIII", 18)]
+                [TestCase("XL", 40)]
+                [TestCase("XLI", 41)]
+                [TestCase("XLIV", 44)]
+                [TestCase("XLIX", 49)]
+
+                [TestCase("XCIX", 99)]
+
+                [TestCase("MCMXCIV", 1994)]
+
+                [TestCase("MMMCMXCIX", 3999)]
+                public void GivenX_ReturnsY(string numerals, int expected)
+                {
+                    //arrange
+                    var roman = new Roman();
+                    //act
+                    var actual = roman.ToInteger(numerals);
+                    //assert
+                    Assert.That(actual, Is.EqualTo(expected));
+                }
+            }
         }
     }
 }
--- source/roman.numerals/Roman.cs
+++ source/roman.numerals/Roman.cs
@@ -7,7 +7,7 @@ namespace roman.numerals
 {
     public class Roman
     {
-        private readonly Dictionary<int, string> _map = new Dictionary<int, string>
+        private readonly Dictionary<int, string> _mapDecimalToRoman = new Dictionary<int, string>
         {
             { 1, "I" },
             { 5, "V" },
@@ -17,37 +17,51 @@ namespace roman.numerals
             { 500, "D" },
             { 1000, "M" }
         };
-        private readonly List<int> _checkpoints = null;
+        private Dictionary<string, int> _mapRomanToDecimal = null;
+        private List<int> _decimalCheckpoints = null;
 
         public Roman()
         {
-            CreateLowerboundCheckpoints();
-
-            _checkpoints = _map
-                    .Keys
-                    .OrderByDescending(x => x)
-                    .ToList();
+            CreateDoubleRomanNumeralMaps();
+            CreateRomanToDecimalMaps();
+            CreateListOfDecimalCheckpoints();
         }
 
-        private void CreateLowerboundCheckpoints()
+        private void CreateDoubleRomanNumeralMaps()
         {
             var newMaps = new List<KeyValuePair<int, string>>();
-            foreach (var map in _map)
+            foreach (var map in _mapDecimalToRoman)
             {
-                var lowerThanMe = _map
+                var lowerThanMe = _mapDecimalToRoman
                     .Where(m => m.Key < (map.Key / 2))
                     .OrderByDescending(m => m.Key)
                     .FirstOrDefault();
 
                 if (lowerThanMe.Key == 0) continue;
-                
+
                 newMaps.Add(new KeyValuePair<int, string>(map.Key - lowerThanMe.Key, $"{lowerThanMe.Value}{map.Value}"));
             }
 
             foreach (var newMap in newMaps)
             {
-                _map.Add(newMap.Key, newMap.Value);
+                _mapDecimalToRoman.Add(newMap.Key, newMap.Value);
             }
+        }
+
+        private void CreateRomanToDecimalMaps()
+        {
+            _mapRomanToDecimal = _mapDecimalToRoman
+                .OrderByDescending(m => m.Value.Length)
+                .ThenBy(m => m.Key)
+                .ToDictionary(m => m.Value, m => m.Key);
+        }
+
+        private void CreateListOfDecimalCheckpoints()
+        {
+            _decimalCheckpoints = _mapDecimalToRoman
+                    .Keys
+                    .OrderByDescending(x => x)
+                    .ToList();
         }
 
         public string ToRomanNumerals(int decimalValue)
@@ -58,19 +72,48 @@ namespace roman.numerals
             var result = new StringBuilder();
             while (decimalValue > 0)
             {
-                var checkpoint = _checkpoints
+                var checkpoint = _decimalCheckpoints
                     .Where(c => c <= decimalValue)
                     .FirstOrDefault();
 
-                var romanValue = _map[checkpoint];
+                var romanValue = _mapDecimalToRoman[checkpoint];
 
                 decimalValue = decimalValue - checkpoint;
 
                 result.Append(romanValue);
-
             }
 
             return result.ToString();
+        }
+
+        public int ToInteger(string romanNumerals)
+        {
+            var totalValue = 0;
+            var index = 0;
+            while (index < romanNumerals.Length)
+            {
+                var singleNumeral = romanNumerals.Substring(index, 1);
+                var doubleNumeral = (index + 1 < romanNumerals.Length)
+                    ? romanNumerals.Substring(index, 2)
+                    : "";
+                
+                if (_mapRomanToDecimal.TryGetValue(doubleNumeral, out var doubleValue))
+                {
+                    totalValue += doubleValue;
+                    index += 2;
+                } 
+                else if (_mapRomanToDecimal.TryGetValue(singleNumeral, out var singleValue))
+                {
+                    totalValue += singleValue;
+                    index += 1;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(romanNumerals), $"{singleNumeral} is not a valid numeral");
+                }
+            }
+            
+            return totalValue;
         }
     }
 }
